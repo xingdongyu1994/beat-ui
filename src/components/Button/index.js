c5c85bba836c4e71a75afea4d7c890f92c9eadd8
xingdongyu <1546258060@qq.com>
2019-05-22 14:31:33 +0800

    init Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,21 +2,23 @@ import React from 'react'
 import clasname from "classnames"
 import './index.less'
 export default class Button extends React.Component {
-
   render() {
     const  {
       type, // 类型
       children,  // 文本内容
+      disabled,  // 是否可点击
+      onClick,   // 点击事件
     } = this.props
-    console.log("个人供热其他人", this.props)
+    const isDisabled = disabled ? { disabled: true } : { onClick } //是否点击
     const baseProps = {
+      ...isDisabled,
       className: clasname({
         [`learn-btn`]:'learn-btn',
         [`learn-btn-${type}`]: type,
-       
+        [`learn-btn-${disabled}`]: disabled
       })
     }
-    const textContent = children
+    const textContent =  <span>{children}</span>
     return <button {...baseProps}>{textContent}</button>
   }
 }
