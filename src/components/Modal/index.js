e27b315dbfb4e6fe8796f75a0e11feb8d26667ae
xingdongyu <1546258060@qq.com>
2019-05-23 22:39:29 +0800

    判断 modal组件 是通过static调用还是通过组件调用

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,11 +1,40 @@
 import React, { PureComponent }from 'react'
+import { render, createPortal, unmountComponentAtNode } from "react-dom";
 import clasname from "classnames"
 import Button from "../Button";
 import './index.less'
 export default class Modal extends PureComponent {
-  handCancel =() => {
-    this.props.onCancel();
+  state = {
+    mvisible: true,
+  };
+  static renderElementMethod(options) {
+    const container = document.createElement("div");
+    const newmodal = render(
+      <Modal
+        visible
+        {...options}
+      />,container
+    )
+    newmodal._container = document.getElementById('beat-modal-mask');
   }
+  static warning(options) {
+    return this.renderElementMethod(options)
+  }
+  handCancel =() => {
+    const { isStatic } = this.props
+    if(isStatic) {
+      this.setState({
+        mvisible: false
+      },()=> {
+        this.destroy();
+      })
+    } else {
+      this.props.onCancel();
+    }
+  }
+  destroy = () => {
+    this._container.remove()
+  };
   handOk =() => {
     this.props.onCancel();
   }
@@ -33,12 +62,15 @@ export default class Modal extends PureComponent {
       children,  // 子元素
       okText,  // 确认按钮文案
       cancelText, // 取消按钮文案
-      visible, //是否显示隐藏
+      
+      content, // 静态方法内容
+      isStatic, // 是否静态方法
     } = this.props
-    console.log("官方格瑞丰", this.state)
-   console.log("官方发射", this.props)
-    return(
+    const  { mvisible } = this.state
+    const visible = isStatic ? mvisible : this.props.visible;
+    return createPortal(
       <div
+        id = 'beat-modal-mask'
         className ={clasname({
           [`beat-modal-mask`]: `beat-modal-mask`,
           [`beat-modal-mask-${visible}`]: `${visible}`,
@@ -61,24 +93,41 @@ export default class Modal extends PureComponent {
                 [`beat-modal-content`]: 'beat-modal-content'
               })}
             >
-              <div className={`beat-modal-header`}>
-                <div className={`beat-modal-title`}>{title}</div>
+            {
+              isStatic
+              ? <div>
+                <div className={`beat-modal-body`}>
+                  <span className={`beat-modal-confirm-title`}>{title}</span>
+                  {content}
+                  <div className={`beat-modal-confirm-btns`}>
+                    <Button type='primary' onClick={this.handCancel}>知道了</Button>
+                  </div>
+                </div>
               </div>
-              <div className={`beat-modal-body`}>
-                {children}
+            : <div>
+                <div className={`beat-modal-header`}>
+                  <div className={`beat-modal-title`}>{title}</div>
+                </div>
+                <div className={`beat-modal-body`}>
+                  {children}
+                </div>
+                <div className={`beat-modal-footer`}>
+                  <Button onClick={this.handCancel}>
+                    {cancelText}
+                  </Button>
+                  <Button onClick={this.handOk} type='primary'>
+                    {okText}
+                  </Button>
+                </div>
               </div>
-              <div className={`beat-modal-footer`}>
-                <Button onClick={this.handCancel}>
-                  {cancelText}
-                </Button>
-                <Button onClick={this.handOk} type='primary'>
-                  {okText}
-                </Button>
-              </div>
+            }
+           
+              
             </div>
           </div>
         </div>
-      </div>
+      </div>,
+      document.getElementById('root')
     )
   }
 }
@@ -88,4 +137,7 @@ Modal.defaultProps = {
   cancelText: '取消',
   title: 'Basic Modal',
   visible: true,
+  onOk: () => {},
+  onCancel: () => {},
+  isStatic: true
 };
