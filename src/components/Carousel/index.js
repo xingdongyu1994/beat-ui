1de632874c083c555ea126cda12bbb45b9a1426b
xingdongyu <1546258060@qq.com>
2019-05-25 18:19:56 +0800

    添加 base 轮播图组件

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -2,38 +2,101 @@ import React from 'react';
 import clasname from 'classnames';
 import './index.less';
 export default class Carousel extends React.Component {
+  state ={
+    currIndex: 0,
+    itemWidth: this.props.style.width,
+    totalLen:0,
+  }
   handClick =(e) => {
     const {target,} = e;
+    const {itemWidth,currIndex,} = this.state;
     if(target.nodeName === 'BUTTON'){
       console.log(target.innerHTML);
       const clickIndex = parseInt(target.innerHTML, 10);
-      const offset = 400* clickIndex;
+
+      const offset = itemWidth* (currIndex - clickIndex);
+      console.log('方法热', offset);
       this.animate(offset);
+      this.setState({
+        currIndex: clickIndex,
+      });
     }
   }
   animate =(offset) => {
+    const {itemWidth,totalLen,} = this.state;
     const list = document.getElementById('beat-slick-list');
-    const newLeft = -offset;
-    console.log('发生发的撒范德萨', newLeft);
-    list.style.left = newLeft + 'px';
-    list.style.transition = '300ms ease';
+    const newLeft = parseInt(list.style.left?list.style.left:0) +offset;
+    this.setState({
+      currIndex: Math.abs(newLeft/itemWidth),
+    },()=>{
+      list.style.left = newLeft + 'px';
+      list.style.transition = '300ms ease';
+      if(newLeft<=-(itemWidth*totalLen)){
+        this.setState({
+          currIndex: 0,
+        });
+        list.style.left = 0 +'px';
+      }
+    });
+
+  }
+  autoplay = () => {
+    const {itemWidth,} = this.state;
+    const that = this;
+    this.timer = setInterval(function(){
+      that.animate(-itemWidth);
+    }, 2000);
+  }
+  stopPlay =() => {
+    clearInterval(this.timer);
+  }
+  componentDidMount() {
+    const {autoplay,children,style,} = this.props;
+    console.log('范围分为', style);
+    if(autoplay) {
+      this.autoplay();
+    }
+    this.setState({
+      totalLen:children.length,
+    });
+  }
+  handMouseOver =() => {
+    const {autoplay,} = this.props;
+    if(autoplay) {
+      this.stopPlay();
+    }
+  }
+  handMouseOut =() => {
+    const {autoplay,} = this.props;
+    if(autoplay) {
+      this.autoplay();
+    }
   }
   render() {
     const {
-      children,
+      children, // 每一屏
+      dots,  // 是否显示面板指示点
+      style, // 控制大小
     } =this.props;
+    console.log('发顺丰三', this.props);
+    const that = this;
     const childrenList = children.map(function(item,index){
       return <div
         className={clasname({
           ['slick-slider']: 'slick-slider',
         })}
         key={index}
+        style={style}
       >
         {item}
       </div>;
     });
     const childrenUl = children.map(function(item,index){
+      const {currIndex,} = that.state;
       return <li
+        className={clasname({
+          ['slick-active']: index===currIndex ? 'slick-active' : '',
+        })}
         key={index}
       >
         <button>{index}</button>
@@ -49,26 +112,44 @@ export default class Carousel extends React.Component {
           className={clasname({
             ['beat-slick-slider']: 'beat-slick-slider',
           })}
+          id="beat-slick-slider"
+          onMouseOut={this.handMouseOut}
+          onMouseOver={this.handMouseOver}
+          style={style}
         >
           <div
             className={clasname({
               ['beat-slick-list']: 'beat-slick-list',
             })}
             id="beat-slick-list"
+            style={{'width':children.length*style.width,}}
           >
             {childrenList}
           </div>
-          <ul
-            className={clasname({
-              ['slick-dots']: 'slick-dots',
-            })}
-            onClick={this.handClick}
-          >
-            {childrenUl}
-          </ul>
+          {
+            dots
+              ? <ul
+                className={clasname({
+                  ['slick-dots']: 'slick-dots',
+                })}
+                onClick={this.handClick}
+              >
+                {childrenUl}
+              </ul>
+              : ''
+          }
+
         </div>
       </div>
     );
     return carousel;
   }
 }
+Carousel.defaultProps = {
+  autoplay: false,
+  dots: true,
+  style: {
+    width:400,
+    height:200,
+  },
+};
